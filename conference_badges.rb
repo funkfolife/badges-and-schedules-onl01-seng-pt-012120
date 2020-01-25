efbbdd729ef4b1139d7dc58daa6cb08df7ed0584
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-25 12:51:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,6 @@
-
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-
-
 def badge_maker(name)
   return "Hello, my name is #{name}."
   end
+  
+def batch_badge_creator(attendees)
   
