1d059ea5e8872b2647110c42d96884bbd174fa30
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-25 12:59:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,7 @@ def badge_maker(name)
 def batch_badge_creator(attendees)
    attendees_badges = []
     attendees.each do |name|
-      attendees_badges.push("hell, my name is #{name}.")
+      attendees_badges.push("Hello, my name is #{name}.")
   end
+  return attendees_badges
 end 
