e28433ad00862a5e197ae2b76b05cb9303e8cf60
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-25 13:35:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,13 +20,13 @@ def assign_rooms(attendees)
   room_assignments 
 end
 
-#def printer(attendees)
- # batch_badge_creator(attendees).each do |badge|
-  #  puts |badge|
-  #end
-  #assign_rooms(attendees).each do |assignment|
-   # puts |assignment|
-  #end
-#end
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
   
   
